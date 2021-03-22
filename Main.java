e600c306cace628ef9ff27c366d7c6b339c8ed29
ashbrojb <ashbrojb@mail.uc.edu>
2021-03-22 16:20:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/* My partner for this lab is Chris Fenter*/
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class Main {
+  public static void main(String args[]){
+    SwingUtilities.invokeLater(new Runnable(){
+      public void run() {
+        new Counter();
+      }
+    });
+  }
+}
